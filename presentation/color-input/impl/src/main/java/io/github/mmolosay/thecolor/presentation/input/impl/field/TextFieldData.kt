784698c7d067440e10ce13503b2af44b8267e8b7
mmolosay <mmolosays@gmail.com>
2025-01-06 15:20:24 +0100

    Refine KDoc

--- presentation/color-input/impl/src/main/java/io/github/mmolosay/thecolor/presentation/input/impl/field/TextFieldData.kt
+++ presentation/color-input/impl/src/main/java/io/github/mmolosay/thecolor/presentation/input/impl/field/TextFieldData.kt
@@ -4,7 +4,7 @@ package io.github.mmolosay.thecolor.presentation.input.impl.field
  * Platform-agnostic data provided by ViewModel to 'Text Field' View.
  *
  * @param onTextChange a callback to be invoked when displayed text is changed.
- * It should receive a result of [filterUserInput] or other text that is presentation-ready.
+ * It should receive a result of [filterUserInput] or other [Text] that is presentation-ready.
  *
  * @param filterUserInput filters text from user input and returns processed text to be displayed.
  */
@@ -20,11 +20,11 @@ data class TextFieldData(
      * Can be obtained from [filterUserInput].
      *
      * The main point of having this `value class` is to avoid accidents with data types.
-     * Function [filterUserInput] takes String from UI and returns filtered [Text].
+     * Function [filterUserInput] takes [String] from UI and returns filtered [Text].
      * At the same time, [onTextChange] takes [Text], not String.
      * This depicts that [onTextChange] should receive preemptively filtered text from [filterUserInput].
      */
-    // I decided to not introduce 'Text.Empty' extension for the same reason why there's no String.Empty
+    // I decided to not introduce 'Text.Empty' extension for the same reason why there's no 'String.Empty'
     @JvmInline
     value class Text(val string: String)
 
